80be1f373c9b5d061fec050f07955373c728ab9f
Patty Case <beadypatty@yahoo.com>
2018-09-10 16:43:00 -0700

    Initial Commit

---
+++ RabbitHavenServices/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RabbitHavenServices
+//
+//  Created by Patty Case on 9/10/18.
+//  Copyright © 2018 Azure Horse Creations. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
