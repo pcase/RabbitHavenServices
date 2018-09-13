f60a40c60ea165eb9dfbf3487ed82568241bad80
Patty Case <beadypatty@yahoo.com>
2018-09-12 17:00:53 -0700

    Added stepper code to nail trim page

--- RabbitHavenServices/NailTrimViewController.swift
+++ RabbitHavenServices/NailTrimViewController.swift
@@ -12,10 +12,25 @@ class NailTrimViewController: UIViewController {
 
     @IBOutlet weak var nextButton: UIButton!
     
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    @IBOutlet weak var rabbitLabel: UILabel!
+    
+    @IBOutlet weak var stepper: UIStepper!
+    
+    @IBOutlet weak var donationLabel: UILabel!
+    
+    @IBOutlet weak var durationLabel: UILabel!
+    
+    @IBOutlet weak var usdLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        stepper.wraps = true
+        stepper.autorepeat = true
+        stepper.minimumValue = 1
+        stepper.maximumValue = 6
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,7 +38,27 @@ class NailTrimViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-     // MARK: - Navigation
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        let quantity : Int = Int(sender.value)
+        let rabbitText : String = "Rabbit"
+        let donation : Int = 5
+        let duration : Int = 10
+        let USD : Float = 5.00
+        numberLabel.text = Int(sender.value).description
+        if (quantity > 1) {
+            rabbitLabel.text = rabbitText + "s"
+            donationLabel.text = "$" + String(quantity * donation)
+            durationLabel.text = String(quantity * duration)
+            usdLabel.text = String(format: "%.2f", Float(quantity) * USD)
+        } else {
+            rabbitLabel.text = rabbitText
+            donationLabel.text = "$" + String(donation)
+            durationLabel.text = String(duration)
+            usdLabel.text = String(format: "%.2f", USD)
+        }
+    }
+    
+    // MARK: - Navigation
 
     /*
    
